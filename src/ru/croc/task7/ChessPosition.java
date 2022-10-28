3765d2bea170ec16341f787ec27dc005444535a6
”Egor <dronovegorvl@yandex.com>
2022-10-28 14:41:36 +0300

    Еще раз исправил задание 7

--- src/ru/croc/task7/ChessPosition.java
+++ src/ru/croc/task7/ChessPosition.java
@@ -1,6 +1,5 @@
 package ru.croc.task7;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import ru.croc.task7.exeptions.IllegalPositionException;
 
@@ -23,16 +22,22 @@ public class ChessPosition {
      */
     public static ChessPosition parse(String position) throws IllegalPositionException {
         try {
+
             String[] positions = position.split("");
             if (positions.length != 2) {
                 throw new Exception();
             }
+
+            // перевод позиций в систему координат
             int positionY = Arrays.asList(letterColumns).indexOf(positions[0]);
             int positionX = Integer.parseInt(positions[1]) - 1;
+
             if (positionX < 0 || positionX > 7) {
                 throw new Exception();
             }
+
             return new ChessPosition(positionX, positionY);
+
         } catch (Exception exception) {
             throw new IllegalPositionException(position);
         }
--- src/ru/croc/task7/Task7.java
+++ src/ru/croc/task7/Task7.java
@@ -1,8 +1,7 @@
 package ru.croc.task7;
 
 import java.util.Scanner;
-import ru.croc.task7.exeptions.IllegalMoveException;
-import ru.croc.task7.exeptions.IllegalPositionException;
+import ru.croc.task7.exeptions.*;
 
 public class Task7 {
 
@@ -12,15 +11,19 @@ public class Task7 {
         String[] customPositions = scanner.nextLine().split(" ");
         ChessPosition[] positions = new ChessPosition[customPositions.length];
         try {
+
             // Создаем позиции
             for (int i = 0; i < customPositions.length; i++) {
                 positions[i] = ChessPosition.parse(customPositions[i]);
             }
+
             // Проверяем можели ли так двигаться ход
             for (int j = 0; j < positions.length - 1; j++) {
                 move(positions[j], positions[j + 1]);
             }
+            // если не возникло ошибок, значит движение возможно
             System.out.println("OK");
+
         } catch (IllegalPositionException | IllegalMoveException e) {
             System.err.println(e.getMessage());
         }
@@ -34,11 +37,15 @@ public class Task7 {
      * @throws IllegalMoveException
      */
     public static void move(ChessPosition chessPositionOut, ChessPosition chessPositionIn) throws IllegalMoveException {
+
+        // движение коня по гипотенузе прямоугольного трегольника
         int dy = chessPositionIn.getPositionY() - chessPositionOut.getPositionY();
         int dx = chessPositionIn.getPositionX() - chessPositionOut.getPositionX();
+
         if (Math.pow(dy, 2) + Math.pow(dx, 2) != 5) {
             throw new IllegalMoveException(chessPositionOut, chessPositionIn);
         }
+
     }
 
 }
