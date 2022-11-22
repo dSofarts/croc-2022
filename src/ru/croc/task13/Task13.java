8aecde908ff0569b98c967d6c8007358fe3a0f56
”Egor <dronovegorvl@yandex.com>
2022-11-22 23:59:57 +0300

    Первая рабочая версия задания 13

--- src/ru/croc/task13/Task13.java
+++ src/ru/croc/task13/Task13.java
@@ -8,9 +8,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Task13 {
 
@@ -18,18 +20,78 @@ public class Task13 {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите вашу историю просмтров: ");
-        String userMovieHistory = scanner.nextLine();
+        String userMovie = scanner.nextLine();
+
         String pathToMovies = "/Users/dsofarts/IdeaProjects/croc-2022/src/ru/croc/task13/files/movies.txt";
         String pathToHistory = "/Users/dsofarts/IdeaProjects/croc-2022/src/ru/croc/task13/files/movieWatchHistory.txt";
 
-        Map<Integer, String> availableMovies = readListOfAvailableMovies(pathToMovies);
-        List<List<Integer>> historyMovies = readMovieWatchHistoryDatabase(pathToHistory);
-        List<Integer> history = movieWatchHistoryParser(userMovieHistory);
+        Map<Integer, String> moviesToWatch = readListOfAvailableMovies(pathToMovies);
+        List<List<Integer>> allUsersMovieHistory = readMovieWatchHistoryDatabase(pathToHistory);
+        List<Integer> userMovieHistory = movieWatchHistoryParser(userMovie);
 
-        System.out.println(Collections.singletonList(history));
-        System.out.println(Collections.singletonList(availableMovies));
-        System.out.println(Collections.singletonList(historyMovies));
+        int recommendationFilm = findMovieRecommendations(userMovieHistory, allUsersMovieHistory);
+        if (recommendationFilm != 0) {
+            System.out.println(moviesToWatch.get(recommendationFilm));
+        } else {
+            System.out.println("Увы! Рекомендации отсутствуют!");
+        }
 
+    }
+
+    /**
+     * Поиск рекомендаций фильмов для пользователя
+     * @param userMovieHistory просмотренные фильмы пользователем
+     * @param allUsersMovieHistory база данных просмотров всех пользователей
+     * @return код рекомендуемого фильма
+     */
+    public static int findMovieRecommendations(List<Integer> userMovieHistory,
+            List<List<Integer>> allUsersMovieHistory) {
+
+        // Если не найдется фильм для рекомендаций то выводим ноль
+        int recommendationMovieIndex = 0;
+
+        // Находим все рекомендуемые фильмы для пользователя, которые он не смотрел
+        Set<Integer> allRecommendationMovie = new HashSet<>();
+        for (List<Integer> oneUserHistory : allUsersMovieHistory) {
+            for (int movie : oneUserHistory) {
+                if (userMovieHistory.contains(movie)) {
+                    allRecommendationMovie.addAll(oneUserHistory);
+                    break;
+                }
+            }
+        }
+        allRecommendationMovie.removeAll(userMovieHistory);
+
+        // Ищем самый просматриваемый фильм пользователем
+        Map<Integer, Integer> repeatViews = new HashMap<>();
+        for (int i : userMovieHistory) {
+            Integer count = repeatViews.get(i);
+            repeatViews.put(i, count != null ? count + 1 : 1);
+        }
+        int bestMovie = Collections.max(repeatViews.values());
+
+        // Выбираем наиболее подходящий для просмотра
+        for (int recommendationMovie : allRecommendationMovie) {
+            for (List<Integer> oneUserHistory : allUsersMovieHistory) {
+
+                // Ищем самые популярные фильмы среди сходих пользователей
+                repeatViews.clear();
+                for (int i : oneUserHistory) {
+                    Integer count = repeatViews.get(i);
+                    repeatViews.put(i, count != null ? count + 1 : 1);
+                }
+                int bestUserMovie = Collections.max(repeatViews.values());
+
+                // Если самые популярные схожи то выдаем из фильмов этого человека
+                if (oneUserHistory.contains(recommendationMovie) && bestUserMovie == bestMovie) {
+                    recommendationMovieIndex = recommendationMovie;
+                    return recommendationMovieIndex;
+                }
+            }
+            // Если не было людей которые так же чаще всего смотрят тот же фильм что и пользователь выдаем людой из их просмотренных
+            recommendationMovieIndex = recommendationMovie;
+        }
+        return recommendationMovieIndex;
     }
 
     /**
@@ -56,9 +118,11 @@ public class Task13 {
      * @param path путь к файлу
      * @return List со всей историей просмотров
      */
-    public static List<List<Integer>> readMovieWatchHistoryDatabase(String path) throws IOException {
+    public static List<List<Integer>> readMovieWatchHistoryDatabase(String path)
+            throws IOException {
         List<List<Integer>> movieWatchHistory = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8))) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8))) {
             String history;
             while ((history = reader.readLine()) != null) {
                 movieWatchHistory.add(movieWatchHistoryParser(history));
@@ -76,8 +140,8 @@ public class Task13 {
         List<Integer> movieHistory = new ArrayList<>();
         line = line.replaceAll("[^0-9,]", "");
         String[] allFilmsArray = line.split(",");
-        for (int i = 0; i < allFilmsArray.length; i++) {
-            movieHistory.add(Integer.parseInt(allFilmsArray[i]));
+        for (String film : allFilmsArray) {
+            movieHistory.add(Integer.parseInt(film));
         }
         return movieHistory;
     }
