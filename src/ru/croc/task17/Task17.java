d92f57cecb46ff78d8a9539d6318482e662ea7d6
”Egor <dronovegorvl@yandex.com>
2022-12-09 10:41:31 +0300

    исправил задание 17

--- src/ru/croc/task17/Task17.java
+++ src/ru/croc/task17/Task17.java
@@ -14,24 +14,27 @@ public class Task17 {
     static final String USER = "sa";
     static final String PASSWORD = "sa";
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args)
+            throws ClassNotFoundException, SQLException, IOException {
         Class.forName(DRIVER);
         Driver driver = new org.h2.Driver();
-        try (Connection connection = DriverManager.getConnection(DATABASE, USER, PASSWORD)){
+        try (Connection connection = DriverManager.getConnection(DATABASE, USER, PASSWORD)) {
             createTables(connection);
-            initializationTable("/Users/dsofarts/IdeaProjects/croc-2022/src/ru/croc/task17/table.csv", connection);
-        } catch (SQLException | IOException exception) {
-            exception.printStackTrace();
+            initializationTable(
+                    "/Users/dsofarts/IdeaProjects/croc-2022/src/ru/croc/task17/table.csv",
+                    connection);
         }
     }
 
     /**
      * Создание таблиц
-     * @param connection соединение c БД
+     * @param connection соединение с БД
      */
     public static void createTables(Connection connection) throws SQLException {
-        try (Statement statement = connection.createStatement()){
-            String SQL = "CREATE TABLE \"ORDER\"(NUMBER INT, LOGIN VARCHAR(255), ARTICLE VARCHAR(255));\nCREATE TABLE \"PRODUCT\"(ARTICLE VARCHAR(255) PRIMARY KEY, NAME VARCHAR(255), PRICE INTEGER);".formatted();
+        try (Statement statement = connection.createStatement()) {
+            String SQL = ("CREATE TABLE \"ORDER\"(NUMBER INT, LOGIN VARCHAR(255), ARTICLE "
+                    + "VARCHAR(255));\nCREATE TABLE \"PRODUCT\"(ARTICLE VARCHAR(255) PRIMARY KEY, "
+                    + "NAME VARCHAR(255), PRICE INTEGER);").formatted();
             statement.execute(SQL);
         }
     }
@@ -39,15 +42,17 @@ public class Task17 {
     /**
      * Добавление в таблицу
      * @param connection соединение с БД
-     * @param tableName Имя таблицы
+     * @param tableName  Имя таблицы
      * @param attributes в какие ячейки
-     * @param first первое значение
-     * @param second второе значение
-     * @param third третье значение
+     * @param first      первое значение
+     * @param second     второе значение
+     * @param third      третье значение
      */
-    public static void update(Connection connection, String tableName, String attributes, String first, String second, String third) throws SQLException {
-        try (Statement statement = connection.createStatement()){
-            String SQL = "INSERT INTO %s%s VALUES(%s, %s, %s);".formatted(tableName, attributes, first, second, third);
+    public static void update(Connection connection, String tableName, String attributes,
+            String first, String second, String third) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            String SQL = "INSERT INTO %s%s VALUES(%s, %s, %s);".formatted(tableName, attributes,
+                    first, second, third);
             statement.execute(SQL);
         }
     }
@@ -55,12 +60,12 @@ public class Task17 {
     /**
      * Проверка есть ли артикул в БД
      * @param connection соединение с БД
-     * @param article артикул
+     * @param article    артикул
      * @return наличие артикула
      */
     public static boolean haveArticle(Connection connection, String article) throws SQLException {
         String SQL = "SELECT * FROM PRODUCT WHERE ARTICLE = %s;".formatted(article);
-        try (Statement statement = connection.createStatement()){
+        try (Statement statement = connection.createStatement()) {
             try (ResultSet result = statement.executeQuery(SQL)) {
                 return result.next();
             }
@@ -69,17 +74,20 @@ public class Task17 {
 
     /**
      * Инициализация таблиц
-     * @param pathCSV путь к CSV файлу
+     * @param pathCSV    путь к CSV файлу
      * @param connection соединение с БД
      */
-    public static void initializationTable(String pathCSV, Connection connection) throws IOException, SQLException {
+    public static void initializationTable(String pathCSV, Connection connection)
+            throws IOException, SQLException {
         BufferedReader reader = new BufferedReader(new FileReader(pathCSV));
         String line = reader.readLine();
         while (line != null) {
             String[] cells = line.split(",");
-            update(connection, "\"ORDER\"", " (NUMBER, LOGIN, ARTICLE)", cells[0], "'" + cells[1] + "'","'" +  cells[2] + "'");
-            if(!haveArticle(connection, "'" + cells[2] + "'")) {
-                update(connection, "PRODUCT",  " (ARTICLE, NAME, PRICE)", "'" + cells[2] + "'","'" +  cells[3] + "'", cells[4]);
+            update(connection, "\"ORDER\"", " (NUMBER, LOGIN, ARTICLE)", cells[0],
+                    "'" + cells[1] + "'", "'" + cells[2] + "'");
+            if (!haveArticle(connection, "'" + cells[2] + "'")) {
+                update(connection, "PRODUCT", " (ARTICLE, NAME, PRICE)", "'" + cells[2] + "'",
+                        "'" + cells[3] + "'", cells[4]);
             }
             line = reader.readLine();
         }
